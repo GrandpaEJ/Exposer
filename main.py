e75c2435820968c3224c995578063deafa010052
Grandpa EJ <103351465+GrandpaEJ@users.noreply.github.com>
2025-09-20 16:14:41 +0600

    init main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# ------ imports --------- #
+import http.server
+import socketserver
+import os
+
+PORT = 8000  #-------------------- port
+DIRECTORY = os.getcwd()  #-------- current codespace directory
+
+os.chdir(DIRECTORY)
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
